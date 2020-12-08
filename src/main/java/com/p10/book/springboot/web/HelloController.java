de1145e3f79d35165cbd161940f0ea83a6ab2e2c
ParkIlYoung <gouduck@naver.com>
2020-12-08 15:14:11 +0900

    [삭제] HelloController 내 javafx 삭제

--- src/main/java/com/p10/book/springboot/web/HelloController.java
+++ src/main/java/com/p10/book/springboot/web/HelloController.java
@@ -1,8 +1,6 @@
 package com.p10.book.springboot.web;
 
 import com.p10.book.springboot.web.dto.HelloResponseDto;
-import javafx.application.Application;
-import javafx.stage.Stage;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
